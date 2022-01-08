e6692a3bbe997b98e8a0651ec24c1cfdc4fb31ea
Damian Barrous-Dume <damianbarrous-dume@Damians-MacBook-Pro.local>
2022-01-08 16:46:12 +0000

    Add a bit of styling

--- web/src/views/sharedNote.js
+++ web/src/views/sharedNote.js
@@ -1,66 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { showError, SILENT_ERROR } from '../utilities/error';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { showError, SILENT_ERROR } from "../utilities/error";
+import styled from "styled-components";
 
 const NoteContainer = styled.div`
-    display: flex;
-    flex-flow: column;
-    padding: 2em;
-
-    .note-title {
-        margin: auto;
-        margin-bottom: 1em;
-    }
+  display: flex;
+  flex-flow: column;
+  padding: 2em;
+  width: 70%;
+  margin: 0 auto;
+  .note-title {
+    margin: auto;
+    margin-bottom: 1em;
+  }
 `;
 
 const SharedNote = (props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [noteText, setNoteText] = useState("");
+  const [noteTitle, setNoteTitle] = useState("");
+  const id = searchParams.get("id");
+  const { getAccessTokenSilently } = useAuth0();
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [noteText, setNoteText] = useState('');
-    const [noteTitle, setNoteTitle] = useState('');
-    const id = searchParams.get("id")
-    const { getAccessTokenSilently } = useAuth0();
+  useEffect(() => {
+    const getTags = async () => {
+      try {
+        const token = await getAccessTokenSilently();
 
-    useEffect(() => {
-        const getTags = async () => {
-          try {
-            const token = await getAccessTokenSilently();
-    
-            const response = await fetch(`https://api.cloudnotes.link/sharedNote?n=${id}`, {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-              }
-            });
-            if (response.status === 200) {
-              const responseData = await response.json();
-              if (responseData && responseData.length > 0) {
-                  setNoteText(responseData[0].note_text);
-                  setNoteTitle(responseData[0].title);
-              }
-            } else showError(response.statusText, SILENT_ERROR);
-          } catch (error) {
-            showError(error, SILENT_ERROR);
+        const response = await fetch(
+          `https://api.cloudnotes.link/sharedNote?n=${id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
           }
-        };
-        getTags();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-      
-    console.log(id);
-    return (
-        <NoteContainer>
-            <h1 className="note-title">{noteTitle}</h1>
-            <div 
-                dangerouslySetInnerHTML={{ __html: noteText }} 
-                className="note-text"
-            />
-        </NoteContainer>
+        );
+        if (response.status === 200) {
+          const responseData = await response.json();
+          if (responseData && responseData.length > 0) {
+            setNoteText(responseData[0].note_text);
+            setNoteTitle(responseData[0].title);
+          } else {
+            setNoteTitle("Note Not Found");
+            setNoteText("Try again.");
+          }
+        } else showError(response.statusText, SILENT_ERROR);
+      } catch (error) {
+        showError(error, SILENT_ERROR);
+      }
+    };
+    getTags();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    );
+  console.log(id);
+  return (
+    <NoteContainer>
+      <h1 className="note-title">{noteTitle}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: noteText }}
+        className="note-text"
+      />
+    </NoteContainer>
+  );
 };
 
 export default SharedNote;
